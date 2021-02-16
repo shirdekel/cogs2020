fd0bd123c0b2bab77136e29a83a16a2fd9a6bf4a
Dr Matthew Crossley <mq20185996@hus-cs-1613-os1.local>
2021-02-17 06:59:55 +1100

    getting started

---
+++ make_all.R
@@ -0,0 +1,18 @@
+library(stringr)
+
+setwd('/Users/mq20185996/Dropbox/teaching/2021/cogs2020/live_course_material/')
+out_format <- 'html_document'
+in_files <- list.files(getwd(), pattern='*.Rmd', full.names=T)
+
+make <- function(in_file) {
+    rmarkdown::render(
+                   in_file,
+                   output_format = out_format,
+                   output_file = str_replace(in_file, '.Rmd', '-exported.html'),
+                   knit_root_dir=getwd()
+               )
+}
+
+for(in_file in in_files) {
+    make(in_file)
+}
