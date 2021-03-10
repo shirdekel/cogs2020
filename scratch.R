14b4693c9567e202f3622390048442a63b85acc7
Dr Matthew Crossley <mq20185996@hus-cs-1613-os1.local>
2021-03-10 14:46:09 +1100

    homework 2 et al

--- scratch.R
+++ scratch.R
@@ -1,4 +1,26 @@
-# This is a comment
+library(data.table)
+library(ggplot2)
 
-# Now I will add things
-2 + 2
+rm(list=ls())
+
+x <- c("A", "A", "A", "B", "B", "B" )
+y <- c("I", "I", "II", "II", "III", "III")
+z <- rnorm(6)
+d <- data.table(x, y, z)
+
+ans_1e <- ggplot(data=d, aes(x=x, y=z)) +
+  geom_point()
+ans_1e
+
+d[x=="A", mean(z)]
+d[x=="B", mean(z)]
+
+dd <- d[, mean(z), .(x, y)]
+setnames(dd, "V1", "z")
+
+# shallow copy
+d2 <- d
+
+# deep copy
+d2 <- data.table(d)
+
