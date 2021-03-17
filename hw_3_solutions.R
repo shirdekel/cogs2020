a8cf6ee05dc0d4f11c18177a4ca1381dfbf94b56
Dr Matthew Crossley <mq20185996@hus-cs-1613-os1.local>
2021-03-17 17:27:01 +1100

    working hw3 and some grade updates

---
+++ hw_3_solutions.R
@@ -0,0 +1,23 @@
+library(data.table)
+library(ggplot2)
+
+rm(list=ls())
+
+# 1
+p1 <- 1 - pnorm(2, 0, 1, lower.tail=F)
+p2 <- pnorm(2, 0, 1, lower.tail=T)
+ans_1a <- p1==p2
+p1 <- 1 - pbinom(1, 15, 0.5, lower.tail=F)
+p2 <- pbinom(2, 15, 0.5, lower.tail=T)
+ans_1b <- p1==p2
+
+# 2
+mu <- 15
+sigma <- 2.74
+ans_2a <- pnorm(24.23, mu, sigma, lower.tail=F)
+ans_2b <- pnorm(13.54, mu, sigma, lower.tail=T)
+ans_2c <- pnorm(10.98, mu, sigma, lower.tail=T)
+ans_2d <- pnorm(4.12, mu, sigma, lower.tail=F)
+ans_2e <- pnorm(24.55, mu, sigma, lower.tail=T) - pnorm(12.22, mu, sigma, lower.tail=T)
+ans_2f <- qnorm(0.9, mu, sigma, lower.tail=F)
+ans_2g <- qnorm(0.8, mu, sigma, lower.tail=T)
