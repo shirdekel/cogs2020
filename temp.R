bbf27ba8a514a59c817802fdde8ef8f326a175e2
Dr Matthew Crossley <mq20185996@hus-cs-1613-os1.local>
2021-04-23 17:30:07 +1000

    qf madness

--- temp.R
+++ temp.R
@@ -3,41 +3,48 @@ library(ggplot2)
 
 rm(list=ls())
 
-# HW 4 prob 2
+# HW 4 prob 6
 
 # 1
-# H0: p = 0.5
-# H1: p != 0.5
+# X -> mean accuracy per block
+# X ~ N(mu_x, sig_x)
+# H0: mu_x = 0.5
+# H1: mu_x != 0.5
 
 # 2
 # alpha = 0.05
 
 # 3
-# p_hat = x_obs / n
-# x_obs ~ binomial(n, p) --- count of successes
-# p_hat ~ binomial(n, p), x -> x / n --- proportion of successes 
+# mu_hat = x_bar
+# x_bar ~ N(mu_x_bar, sig_x_bar)
+# mu_x_bar = mu_x
+# sig_x_bar = sig_x / sqrt(n)
+# t_obs = (x_bar_obs - mu_x_bar) / sig_x_bar
+# t_obs ~ t(n-1)
 
 # 4
-# data will be stored in a data.table
-# You've got d and dd from problem 1
-# d contains trial by trial information 
-# dd contains block by block information
-# first 100 trials are in d
-# d[i, j, by]
-# i -> what rows do you want
-# j -> what columns do you want and what to do with them
-# by -> how do you want to group what you're doing with the js
-x_obs <- d[(phase == 1) & (trial %in% 1:100), acc]
+# data is in d2 and dd2
+# d2 carries information about trials
+# dd2 carries anout blocks, so we use dd2
+x_obs = dd2[(phase==1) & (block %in% 1:16), acc_mean]
 n <- length(x_obs)
-p_hat_obs <- sum(x_obs == 1) / n
+x_bar_obs <- mean(x_obs)
+sig_x_bar_hat <- sd(x_obs)
+t_obs <- (x_bar_obs - 0.5) / (sig_x_bar_hat / sqrt(n))
 
 # 5
-# lower.tail=FALSE: P(X > x) = P(X >= x - 1)
-p_val_upper <- pbinom(p_hat_obs_upper * n, n, 0.5, lower.tail=FALSE)
-p_val_lower <- pbinom(p_hat_obs_lower * n, n, 0.5, lower.tail=TRUE)
-p_val <- p_val_lower + p_val_upper
+t_obs_upper <- abs(t_obs)
+t_obs_lower <- -abs(t_obs)
+p_val_upper <- pt(t_obs_upper, n-1, lower.tail=FALSE)
+p_val_lower <- pt(t_obs_lower, n-1, lower.tail=TRUE)
+p_val <- p_val_upper + p_val_lower
 
-crit_upper <- (qbinom(0.025, n, p, lower.tail=FALSE) - 1) / n 
-crit_lower <- (qbinom(0.025, n, p, lower.tail=TRUE)) / n
+t_crit_upper <- qt(0.025, n-1, lower.tail=FALSE)
+t_crit_lower <- qt(0.025, n-1, lower.tail=TRUE)
 
-# binom.test(...)
+ci_width <- (t_crit_upper - t_crit_lower) * sig_x_bar_hat
+ci_upper <- x_bar_obs + ci_width / 2
+ci_lower <- x_bar_obs - ci_width / 2
+
+t.test(x_obs, mu=0.5, alternative='two.sided')
+
