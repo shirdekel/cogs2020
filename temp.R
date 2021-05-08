34cb7c8f083011fb2468d9b78a50e2f1093ce5e9
Dr Matthew Crossley <mq20185996@hus-cs-1613-os1.local>
2021-05-08 19:33:15 +1000

    lecture 8 and homework 6

--- temp.R
+++ temp.R
@@ -1,5 +1,13 @@
 library(data.table)
 library(ggplot2)
+rm(list=ls())
 
-rm(list = ls())
+x <- 0:100
+fx <- dbinom(x, 100, 0.5)
+d <- data.table(x, fx)
+ggplot(d, aes(x, fx)) +
+  geom_point() + 
+  geom_vline(xintercept=82)
 
+pbinom(82-1, 100, 0.5, lower.tail=F)
+d[x >= 82, sum(fx)]
