8b7eb4a6b4c30eb7e3af2318cbc0984e9ac38636
Dr Matthew Crossley <mq20185996@hus-cs-1613-os1.local>
2021-05-17 12:39:38 +1000

    grades

--- temp.R
+++ temp.R
@@ -1,13 +1,5 @@
 library(data.table)
 library(ggplot2)
+library(ez)
 rm(list=ls())
 
-x <- 0:100
-fx <- dbinom(x, 100, 0.5)
-d <- data.table(x, fx)
-ggplot(d, aes(x, fx)) +
-  geom_point() + 
-  geom_vline(xintercept=82)
-
-pbinom(82-1, 100, 0.5, lower.tail=F)
-d[x >= 82, sum(fx)]
