b7ab2cb39c2171e46cb00ba4fc4789c7ad4a988e
Dr Matthew Crossley <mq20185996@hus-cs-1613-os1.local>
2021-05-26 13:22:27 +1000

    grades

--- scratch.R
+++ scratch.R
@@ -3,8 +3,4 @@ library(ggplot2)
 
 rm(list=ls())
 
-d <- fread('https://crossley.github.io/cogs2020/data/mis/mis_data.csv')
-
-d[, unique(subject), .(phase)]
-
-d[, unique(subject), .(group)]
+d <- fread('https://crossley.github.io/cogs2020/data/maze/maze.csv')
